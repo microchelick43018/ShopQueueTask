27f2c61b8df1efde18bfa8fd06f1243cecfd306d
microchelick43018 <dyatlovkonstan@gmail.com>
2021-02-11 22:16:40 +0300

    Удалена newCost, в функцию Enqueue в аргумент заноситься сразу ввод. В Main'e передаетс не сама коллекция, а только её элементы.

--- Program.cs
+++ Program.cs
@@ -8,35 +8,33 @@ namespace ShopQueueTask
         {
             Console.WriteLine("Введите количество покупателей: ");
             int quantityOfClients = Convert.ToInt32(Console.ReadLine());
-            int newCost;
             for(int i = 0; i < quantityOfClients; i++)
             {
-                Console.WriteLine($"Введие цену {i + 1}-ого покупателя");
-                newCost = Convert.ToInt32(Console.ReadLine());
-                costs.Enqueue(newCost);
+                Console.WriteLine($"Введие цену {i + 1}-ого покупателя");              
+                costs.Enqueue(Convert.ToInt32(Console.ReadLine()));
             }
         }
-        
-        static void ServeClients(Queue<int> costs)
+
+        static void ServeClients(int cost, ref int balance)
         {
-            int balance = 0;
-            while(costs.Count != 0)
-            {
-                Console.Clear();
-                Console.WriteLine($"Следующий на очереди покупатель с суммой {costs.Peek()}");
-                balance += costs.Dequeue();
-                Console.WriteLine($"Клиент обслужен! Текущий баланс: {balance}");
-                Console.WriteLine("Нажмите любую клавишу, чтобы продолжить.");
-                Console.ReadKey();
-            }
-            Console.WriteLine("Очередь закончена!");
+            Console.Clear();
+            Console.WriteLine($"Следующий на очереди покупатель с суммой {cost}");
+            balance += cost;
+            Console.WriteLine($"Клиент обслужен! Текущий баланс: {balance}");
+            Console.WriteLine("Нажмите любую клавишу, чтобы продолжить.");
+            Console.ReadKey();     
         }
 
         static void Main(string[] args)
         {
             Queue<int> costs = new Queue<int>();
+            int balance = 0;
             InitQueue(ref costs);
-            ServeClients(costs);
+            while(costs.Count != 0)
+            {
+                ServeClients(costs.Dequeue(), ref balance);
+            }
+            Console.WriteLine("Очередь закончена!");
             Console.ReadKey();
         }
     }
