9979d650906ba4e60061e7f9dc4523b131889d84
microchelick43018 <dyatlovkonstan@gmail.com>
2021-02-13 14:00:33 +0300

    Добавлен чекер (проверяет ввод интовского числа). Передача очереди теперь только по его значениям

--- Program.cs
+++ Program.cs
@@ -4,17 +4,6 @@ namespace ShopQueueTask
 {
     class Program
     {
-        static void InitQueue(ref Queue<int> costs)
-        {
-            Console.WriteLine("Введите количество покупателей: ");
-            int quantityOfClients = Convert.ToInt32(Console.ReadLine());
-            for(int i = 0; i < quantityOfClients; i++)
-            {
-                Console.WriteLine($"Введие цену {i + 1}-ого покупателя");              
-                costs.Enqueue(Convert.ToInt32(Console.ReadLine()));
-            }
-        }
-
         static void ServeClients(int cost, ref int balance)
         {
             Console.Clear();
@@ -25,14 +14,39 @@ namespace ShopQueueTask
             Console.ReadKey();     
         }
 
+        static void InputIntChecker(ref int value)
+        {
+            bool isCorrect = false;
+            while (true)
+            {
+                isCorrect = int.TryParse(Console.ReadLine(), out value);
+                if (!isCorrect)
+                {
+                    Console.WriteLine("Значение введено некорректно! Повторите ввод:");
+                }
+                else
+                {
+                    return;// точка выхода из цикла цикла
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Queue<int> costs = new Queue<int>();
+            Queue<int> values = new Queue<int>();
             int balance = 0;
-            InitQueue(ref costs);
-            while(costs.Count != 0)
+            Console.WriteLine("Введите количество покупателей: ");
+            int quantityOfClients = Convert.ToInt32(Console.ReadLine());
+            int newValue = 0;
+            for (int i = 0; i < quantityOfClients; i++)
+            {                
+                Console.WriteLine($"Введие цену {i + 1}-ого покупателя");
+                InputIntChecker(ref newValue);
+                values.Enqueue(newValue);
+            }
+            while (values.Count != 0)
             {
-                ServeClients(costs.Dequeue(), ref balance);
+                ServeClients(values.Dequeue(), ref balance);
             }
             Console.WriteLine("Очередь закончена!");
             Console.ReadKey();
