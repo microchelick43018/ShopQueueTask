20681eb1ed67bfe317ba8b0f0e91633ed6a1875d
microchelick43018 <dyatlovkonstan@gmail.com>
2021-02-14 13:40:58 +0300

    Коменты убраны, функции переименованы, в функцию ServeClient передается не вся очередь, а её элементы. Переделана проверка.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace ShopQueueTask
 {
     class Program
     {
-        static void ServeClients(int cost, ref int balance)
+        static void ServeClient(int cost, ref int balance)
         {
             Console.Clear();
             Console.WriteLine($"Следующий на очереди покупатель с суммой {cost}");
@@ -14,39 +14,35 @@ namespace ShopQueueTask
             Console.ReadKey();     
         }
 
-        static void InputIntChecker(ref int value)
+        static int ReadInt()
         {
             bool isCorrect = false;
-            while (true)// цикл закончится, когда ввод будет верен, в ином случае будет выдаваться ошибка о вводе
+            int value = 0;
+            isCorrect = int.TryParse(Console.ReadLine(), out value);
+            while (isCorrect == false)
             {
-                isCorrect = int.TryParse(Console.ReadLine(), out value);
-                if (!isCorrect)
-                {
-                    Console.WriteLine("Значение введено некорректно! Повторите ввод:");
-                }
-                else
-                {
-                    return;// точка выхода из цикла и функции
-                }
+                Console.Write("Неккоректный ввод! Повторите ещё раз: ");
+                isCorrect = int.TryParse(Console.ReadLine(), out value);                
             }
+            return value;
         }
 
         static void Main(string[] args)
         {
-            Queue<int> values = new Queue<int>();
+            Queue<int> purchases = new Queue<int>();
             int balance = 0;
             Console.WriteLine("Введите количество покупателей: ");
-            int quantityOfClients = Convert.ToInt32(Console.ReadLine());
-            int newValue = 0;
+            int quantityOfClients = ReadInt();
+            int newPurchase = 0;
             for (int i = 0; i < quantityOfClients; i++)
             {                
-                Console.WriteLine($"Введие цену {i + 1}-ого покупателя");
-                InputIntChecker(ref newValue);
-                values.Enqueue(newValue);
+                Console.Write($"Введите цену покупки {i + 1}-ого клиента: ");
+                newPurchase = ReadInt();
+                purchases.Enqueue(newPurchase);
             }
-            while (values.Count != 0)
+            while (purchases.Count != 0)
             {
-                ServeClients(values.Dequeue(), ref balance);
+                ServeClient(purchases.Dequeue(), ref balance);
             }
             Console.WriteLine("Очередь закончена!");
             Console.ReadKey();
